ead854e8c09f6cd2b194d0e668954407960e94e7
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-07-26 13:57:18 +0600

    add product ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -8,6 +9,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hlsud.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('usedMobileBazar').collection('categories');
+        const productsCollection = client.db('usedMobileBazar').collection('products');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        });
+
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('used mobile bazar is running');
@@ -16,4 +56,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`server is running on ${port}`)
-})
+});
+
+
